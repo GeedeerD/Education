97f27bd1991fc9c73745dc8a1825e67efa52b42e
Vladimir <geedeerdfal@gmail.com>
2025-03-16 12:30:24 +0200

    test class

--- Enums/Types/Color.cs
+++ Enums/Types/Color.cs
@@ -1,4 +1,6 @@
-﻿namespace Enums.Types
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Enums.Types
 {
     public enum ColorFlag : int
     {
@@ -9,5 +11,43 @@
         Pink = 16,
         Red = 32,
         Green = 64,
+    }
+
+
+    class TestPen
+    {
+        public int LengthMm { get; set; }
+        public ColorFlag Color { get; set; }
+        public Form Form { get; set; }
+    }
+
+    class Printer3D : TestPen
+    {
+        public Printer3D():base()
+        {
+        }
+
+        public int Temperature { get; set; }
+    }
+
+    class Program3D
+    {
+        void TestPrint()
+        {
+            var pen = new TestPen();
+            pen.Color = ColorFlag.Blue;
+
+
+            var printer3D = new Printer3D();
+            printer3D.Temperature = -44;
+        }
+    }
+}
+
+namespace Enums
+{
+    enum Form
+    {
+
     }
 }
