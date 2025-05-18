3dea431c6eb87d8112e2f041685fde0691a348cb
Vladimir <geedeerdfal@gmail.com>
2025-05-18 12:22:29 +0300

    simple app

---
+++ MobileFirst/ImagePickerPopup.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MobileFirst;
+
+public partial class ImagePickerPopup : Popup
+{
+    public ImagePickerPopup()
+    {
+        InitializeComponent();
+    }
+
+    private void OnImageSelected(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() is ImageSource selected)
+        {
+            Close(selected); // возвращаем выбранное изображение
+        }
+    }
+}
