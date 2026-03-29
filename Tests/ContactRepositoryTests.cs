e49a85034389378030ba6e77209bbba47e08d647
Vladimir <geedeerdfal@gmail.com>
2026-03-29 13:04:34 +0300

    Revert "change-28-03v"
    This reverts commit 5d1e08df43f0245599f813097a9c4b1ec21f7c64.

--- Tests/ContactRepositoryTests.cs
+++ Tests/ContactRepositoryTests.cs
@@ -89,7 +89,6 @@ namespace Tests
             var contactAfterRemove = await _repo.GetContactByObjectIdAsync(contact.ContactListId, contact.Id);
             Assert.That(contactAfterRemove, Is.Null);
         }
-        //112
 
         [Test]
         public async Task CreateContactAsync_WhenContactAlreadyExists_ShouldEnterExistingContact()
